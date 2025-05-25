580704acc1c68f9fa8ce22cad4d1988410dada2e
Anwesh43 <anweshthecool0@gmail.com>
2025-05-25 23:31:42 +0530

    adding classify signature

--- dspy_starter/app.py
+++ dspy_starter/app.py
@@ -1,16 +1,25 @@
 import dspy
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 
 
 class DspyWrapper:
     def __init__(self, model_name: str):
-        self.llm = dspy.lm(model_name, os.environ["OLLAMA_URL"])
+        # print(os.environ)
+        self.llm = dspy.LM(model_name, api_key=os.environ["OPEN_AI_API_KEY"])
         dspy.configure(lm=self.llm)
 
     def setupSignature(self):
         self.classify = dspy.Predict('sentence -> sentiment:bool')
 
     def classifySentence(self, word: str):
-        print(self.classify(word))
+        response = self.classify(sentence=word)
+        return response.sentiment
+
+
+dspyWrapper = DspyWrapper('openai/gpt-4o-mini')
+dspyWrapper.setupSignature()
+print(dspyWrapper.classifySentence(
+    "it's a charming and often affecting journey."))
